69c83bebcf8a5d8d239da9fcaca0a524061070ac
sander <sander@droneslab.com>
2020-11-29 19:31:01 +0100

    Don't scale max throttle

--- src/modules/fw_pos_control_l1/FixedwingPositionControl.cpp
+++ src/modules/fw_pos_control_l1/FixedwingPositionControl.cpp
@@ -1953,7 +1953,7 @@ FixedwingPositionControl::tecs_update_pitch_throttle(float alt_sp, float airspee
 				const float eas2tas = sqrtf(CONSTANTS_STD_PRESSURE_MBAR / baro.pressure);
 				const float scale = constrain((eas2tas - 1.0f) * _parameters.throttle_alt_scale + 1.0f, 1.0f, 2.0f);
 
-				throttle_max = constrain(throttle_max * scale, throttle_min, 1.0f);
+                                throttle_max = constrain(throttle_max, throttle_min, 1.0f);
 				throttle_cruise = constrain(throttle_cruise * scale, throttle_min + 0.01f, throttle_max - 0.01f);
 			}
 		}
