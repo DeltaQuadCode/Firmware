cd1e95473cbb9df5c92ca9ec1a29f32c110d3a77
sander <sander@droneslab.com>
2020-11-29 19:49:09 +0100

    Widen quadchute tolerance

--- src/modules/vtol_att_control/vtol_type.cpp
+++ src/modules/vtol_att_control/vtol_type.cpp
@@ -292,13 +292,13 @@ void VtolType::check_quadchute_condition()
 
 			// We use tecs for tracking in FW and local_pos_sp during transitions
 			if (_tecs_running) {
-				// 1 second rolling average
-				_ra_hrate = (49 * _ra_hrate + _tecs_status->flight_path_angle) / 50;
-				_ra_hrate_sp = (49 * _ra_hrate_sp + _tecs_status->flight_path_angle_sp) / 50;
+                                // 3 second rolling average
+                                _ra_hrate = (179 * _ra_hrate + _tecs_status->flight_path_angle) / 180;
+                                _ra_hrate_sp = (179 * _ra_hrate_sp + _tecs_status->flight_path_angle_sp) / 180;
 
 				// are we dropping while requesting significant ascend?
 				if (((_tecs_status->altitude_sp - _tecs_status->altitude_filtered) > _params->fw_alt_err) &&
-				    (_ra_hrate < -1.0f) &&
+                                    (_ra_hrate < -1.5f) &&
 				    (_ra_hrate_sp > 1.0f)) {
 
 					_attc->abort_front_transition("QuadChute: loss of altitude");
